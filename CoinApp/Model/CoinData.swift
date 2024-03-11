91cd82613386a881216831dab32f7414d0d1734a
salih-web <bulut._bulut@icloud.com>
2024-03-11 04:33:24 +0300

    Layout yapıldı.

---
+++ CoinApp/Model/CoinData.swift
@@ -0,0 +1,8 @@
+//
+//  CoinData.swift
+//  CoinApp
+//
+//  Created by Muhammed Salih Bulut on 11.03.2024.
+//
+
+import Foundation
---
+++ CoinApp/Model/CoinManager.swift
@@ -0,0 +1,8 @@
+//
+//  CoinManager.swift
+//  CoinApp
+//
+//  Created by Muhammed Salih Bulut on 11.03.2024.
+//
+
+import Foundation
